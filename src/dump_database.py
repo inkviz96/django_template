57cc16adc2ec7d9c0fce2e2d05d4cf696b82893a
Semyon <b-semen-b@mail.ru>
2022-05-06 17:58:13 +0300

    hotfix print

--- src/dump_database.py
+++ src/dump_database.py
@@ -1,16 +1,14 @@
+import logging
 import subprocess
 from datetime import datetime
+
 import dramatiq
 
 
 @dramatiq.actor(max_retries=0)
-def dump():
+def dump() -> None:
     date = datetime.now()
-    # bashCommand = "chmod +x ./dump.sh"
-    # process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
-    # output, error = process.communicate()
-    # print(output, error)
-    bashCommand = f"./dump.sh {date}"
-    process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
+    bash_command: str = f"./dump.sh {date}"
+    process = subprocess.Popen(bash_command.split(), stdout=subprocess.PIPE)
     output, error = process.communicate()
-    print(output, error)
+    logging.info(output, error)
